ca858f82cf761534e1b33c6a04688b4606919b40
Serhat Ege Okuyucu <47014302+sokuyucu@users.noreply.github.com>
2020-12-28 11:48:37 +0300

    Update variables.tf

--- hetzner/variables.tf
+++ hetzner/variables.tf
@@ -1,12 +1,12 @@
-variable "kae_hcloud_token" {
+variable "hcloud_token" {
   default = "<your token>"
 }
 
-variable "kae_cluster_name" {
+variable "cluster_name" {
   description = "prefix for cloud resources"
 }
 
-variable "kae_worker_os" {
+variable "worker_os" {
   description = "OS to run on worker machines"
 
   # valid choices are:
@@ -16,67 +16,67 @@ variable "kae_worker_os" {
   default = "ubuntu"
 }
 
-variable "kae_ssh_public_key_file" {
+variable "ssh_public_key_file" {
   description = "SSH public key file for kae"
   default     = "~/.ssh/kae.pub"
 }
 
-variable "kae_ssh_port" {
+variable "ssh_port" {
   description = "SSH port to be used to provision instances"
   default     = 22
 }
 
-variable "kae_ssh_username" {
+variable "ssh_username" {
   description = "SSH user, used only in output"
   default     = "root"
 }
 
-variable "kae_ssh_private_key_file" {
+variable "ssh_private_key_file" {
   description = "SSH private key file used to access instances"
   default     = ""
 }
 
-variable "kae_ssh_agent_socket" {
+variable "ssh_agent_socket" {
   description = "SSH Agent socket, default to grab from $SSH_AUTH_SOCK"
   default     = "env:SSH_AUTH_SOCK"
 }
 
 # Provider specific settings
 
-variable "kae_control_plane_type" {
+variable "control_plane_type" {
   default = "cpx11"
 }
 
-variable "kae_control_plane_replicas" {
+variable "control_plane_replicas" {
   default = 3
 }
 
-variable "kae_worker_type" {
+variable "worker_type" {
   default = "cx21"
 }
 
-variable "kae_workers_replicas" {
+variable "workers_replicas" {
   default = 3
 }
 
-variable "kae_lb_type" {
+variable "lb_type" {
   default = "lb11"
 }
 
-variable "kae_datacenter" {
+variable "datacenter" {
   default = "nbg1"
 }
 
-variable "kae_image" {
+variable "image" {
   default = "ubuntu-18.04"
 }
 
-variable "kae_ip_range" {
+variable "ip_range" {
   default     = "10.10.10.0/24"
   description = "ip range to use for private network"
 }
 
-variable "kae_network_zone" {
+variable "network_zone" {
   default     = "eu-central"
   description = "network zone to use for private network"
 }
